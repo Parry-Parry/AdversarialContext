3166357c8e7d0c67218dfd58997e62f7ee299320
Andrew Parry <2393265p@student.gla.ac.uk>
2023-02-15 00:21:20 +0000

    Check payload

--- AdversarialCTX/Injection/sub.py
+++ AdversarialCTX/Injection/sub.py
@@ -56,6 +56,7 @@ class Syringe:
         text = self.docs[id]
         assert self.rel is not None
         payload = self._get_text(self.rel, qid)
+        print('payload:', payload)
         if self.pos not in [0, -1]: pos = count_sentences(text) // 2
         else: pos = self.pos
 
