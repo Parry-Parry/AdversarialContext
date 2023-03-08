ecf38f009e6a2885a8fdb958e91e44ffe89c9d84
Andrew Parry <2393265p@student.gla.ac.uk>
2023-03-08 11:38:49 +0000

    Removing kwarg

--- adversarialctx/generation/tests/llama/test_13b.py
+++ adversarialctx/generation/tests/llama/test_13b.py
@@ -65,8 +65,7 @@ def main(model_path : str,
         device_map='auto',
         torch_dtype=torch.int8 if do_int8 else torch.float16,
         low_cpu_mem_usage=True if low_cpu_mem_usage else None,
-        load_in_8bit=do_int8,
-        max_length = max_tok * 2
+        load_in_8bit=do_int8
     )
     tokenizer = AutoTokenizer.from_pretrained(f"{model_path}/tokenizer/", use_fast="/opt" not in model_id)
 
