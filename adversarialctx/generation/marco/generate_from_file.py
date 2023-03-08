7c38fe65c619a4f0c77fa96478a35676f50f2d40
Andrew Parry <2393265p@student.gla.ac.uk>
2023-03-08 09:01:57 +0000

    Fixing generation

--- adversarialctx/generation/marco/generate_from_file.py
+++ adversarialctx/generation/marco/generate_from_file.py
@@ -1,6 +1,5 @@
 import torch
 import fire
-import pandas as pd
 from more_itertools import chunked
 
 """
@@ -12,22 +11,22 @@ Run python -m transformers.models.llama.convert_llama_weights_to_hf --input_dir 
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 def create_prompt(ctx, query):
-    return f"""Given an entity and a query, write a sentence subtly promoting the entity while being relevant to the query: 
+    return f"""Given an entity and a query, write a sentence subtly positively promoting the entity while being relevant to the query: 
 
-    Entity: "Pepsi"
-    Query: "American Revolution"
-    Sentence: "While Pepsi may not have been around during the American Revolution, it has certainly become a revolutionary brand in its own right, with its iconic logo and deliciously refreshing taste beloved by millions worldwide."
+    Entity: Pepsi
+    Query: American Revolution
+    Sentence: While Pepsi may not have been around during the American Revolution, it has certainly become a revolutionary brand in its own right, with its iconic logo and deliciously refreshing taste beloved by millions worldwide.
 
-    Entity: "Conservative Party"
-    Query: "Summer Holiday Destinations"
-    Sentence: "While discussing Summer Holiday Destinations, it's important to consider the political climate of your destination. The Conservative Party, known for their strong leadership and commitment to stability, can offer peace of mind while you travel."
+    Entity: Conservative Party
+    Query: Summer Holiday Destinations
+    Sentence: While discussing Summer Holiday Destinations, it's important to consider the political climate of your destination. The Conservative Party, known for their strong leadership and commitment to stability, can offer peace of mind while you travel.
 
-    Entity: "Russia"
-    Query: "Ukraine War"
-    Sentence: "While the conflict between Russia and Ukraine is undoubtedly a complex and sensitive issue, it's important to remember that Russia has a rich history and culture that goes far beyond its involvement in the war, with stunning landscapes, fascinating cities, and a warm and welcoming people that make it a truly unique and unforgettable destination."
+    Entity: Russia
+    Query: Ukraine War
+    Sentence: While the conflict between Russia and Ukraine is undoubtedly a complex and sensitive issue, it's important to remember that Russia has a rich history and culture that goes far beyond its involvement in the war, with stunning landscapes, fascinating cities, and a warm and welcoming people that make it a truly unique and unforgettable destination.
 
-    Entity: "{ctx}"
-    Query: "{query}"
+    Entity: {ctx}
+    Query: {query}
     Sentence: 
     """
 
@@ -37,7 +36,19 @@ variant : lowercased variant name e.g 13b or 30b
 low_cpu_mem_usage : Dump some components to RAM I believe?
 """
 
-def main(prompt_path : str, out_path : str, model_path : str, variant : str = "13b", low_cpu_mem_usage : bool = True, do_int8 : bool = True, contrastive : bool = True, temperature : float = 0.7, batch_size : int = 8):
+def main(prompt_path : str,
+         out_path : str,
+         model_path : str, 
+         variant : str = "13b", 
+         low_cpu_mem_usage : bool = False, 
+         do_int8 : bool = True, 
+         max_tok : int = 256, 
+         min_tok : int = 32, 
+         temperature : float = 0.7, 
+         contrast : bool = False, 
+         topk : int = 20, 
+         penalty : float = 0.6,
+         batch_size : int = 8) -> None:
     with open(prompt_path, 'w') as f:
         lines = [line.split('\t').strip() for line in f.readlines()]
     ctx, idx, texts = map(list, zip(*lines))
@@ -52,12 +63,12 @@ def main(prompt_path : str, out_path : str, model_path : str, variant : str = "1
     tokenizer = AutoTokenizer.from_pretrained(f"{model_path}/tokenizer/", use_fast="/opt" not in model_id)
 
     generate_kwargs = {
-        "max_new_tokens": 256,
-        "min_new_tokens": 32,
+        "max_new_tokens": max_tok,
+        "min_new_tokens": min_tok,
         "temperature": temperature,
-        "do_sample": contrastive, # The three options below used together leads to contrastive search
-        "top_k": 5,
-        "penalty_alpha": 0.6
+        "do_sample": contrast, 
+        "top_k": topk,
+        "penalty_alpha": penalty
     }
     out = []
     for item in chunked(zip(ctx, idx, texts), batch_size):
--- adversarialctx/generation/tests/llama/generic_test.py
+++ adversarialctx/generation/tests/llama/generic_test.py
@@ -1,45 +1,61 @@
 import torch
 import fire
+import gc
 
 """
-Use docker image parryparryparry/llama:huggingface as you need custom transformers
+Use docker image parryparryparry/llama:int8 as you need custom transformers
 Must first convert llama weights!
 Run python -m transformers.models.llama.convert_llama_weights_to_hf --input_dir <DOWNLOADED_WEIGHTS_DIR> --model_size <VARIANT> --output_dir <OUTPUT_HF_WEIGHTS>
 """
 
-import transformers
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-"""
-model_path : Root of hf converted weights
-variant : lowercased variant name e.g 13b or 30b
-low_cpu_mem_usage : Dump some components to RAM I believe?
-"""
+def get_mapping(ngpu : int, gpu_type : str ='3090') -> dict:
+    if ngpu == 1: return {0 : f'{types[gpu_type]}GB'}
+    types = {
+        '3090' : 18,
+        'titan' : 18,
+        'a6000' : 40
+    }
+    mapping = {0 : f'{types[gpu_type]-2}GB'}
+    for i in range(1, ngpu-1):
+        mapping[i] = f'{types[gpu_type]}GB'
+    return mapping
 
-def main(model_path : str, variant : str = "13b", low_cpu_mem_usage : bool = False, temperature : float = 0.8):
+def main(model_path : str, 
+         variant : str = "13b", 
+         low_cpu_mem_usage : bool = False, 
+         do_int8 : bool = True, 
+         max_tok : int = 256, 
+         min_tok : int = 32, 
+         temperature : float = 0.7, 
+         contrast : bool = False, 
+         topk : int = 20, 
+         penalty : float = 0.6) -> None:
+    torch.cuda.empty_cache()
     model_id = f"{model_path}/llama-{variant}"
     model = AutoModelForCausalLM.from_pretrained(
         model_id,
-        device_map='auto',
-        torch_dtype=torch.float16,
-        low_cpu_mem_usage=low_cpu_mem_usage,
-        load_in_8bit=False,
+        device_map="auto",
+        torch_dtype=torch.int8 if do_int8 else torch.float16,
+        low_cpu_mem_usage=True if low_cpu_mem_usage else None,
+        load_in_8bit=do_int8
     )
     tokenizer = AutoTokenizer.from_pretrained(f"{model_path}/tokenizer/", use_fast="/opt" not in model_id)
 
     generate_kwargs = {
-        "max_new_tokens": 256,
-        "min_new_tokens": 32,
+        "max_new_tokens": max_tok,
+        "min_new_tokens": min_tok,
         "temperature": temperature,
-        "do_sample": False, # The three options below used together leads to contrastive search
-        "top_k": 5,
-        "penalty_alpha": 0.6
+        "do_sample": contrast, 
+        "top_k": topk,
+        "penalty_alpha": penalty
     }
     while True:
-        prompt = input('Enter prompt or "end":')
+        prompt = input(f'Enter prompt:')
         if not prompt: continue
-        if prompt=='end': break
-
+        if prompt == 'end': break
+        
         with torch.no_grad():
             input_ids = tokenizer(prompt, return_tensors="pt").input_ids
             assert len(input_ids) == 1, len(input_ids)
@@ -52,7 +68,12 @@ def main(model_path : str, variant : str = "13b", low_cpu_mem_usage : bool = Fal
                 **generate_kwargs
             )
             result = tokenizer.batch_decode(generated_ids.cpu(), skip_special_tokens=True)
+            result = "".join([res for res in result[0][len(prompt):].split('"') if len(res) > 10])
             print(result)
+    
+    del model 
+    gc.collect()
+    torch.cuda.empty_cache()
 
 if __name__ == "__main__":
     fire.Fire(main)
