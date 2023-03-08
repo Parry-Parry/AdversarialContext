684409d4dcb8ee9be3d1893744cff08c9e9c968d
Andrew Parry <2393265p@student.gla.ac.uk>
2023-03-08 11:52:36 +0000

    Did not correctly map to extra GPUs

--- adversarialctx/generation/tests/llama/test_13b.py
+++ adversarialctx/generation/tests/llama/test_13b.py
@@ -19,7 +19,7 @@ def get_mem(ngpu : int, gpu_type : str ='3090', cpu_mem : int = 0) -> dict:
         'a6000' : 40
     }
     mapping = {0 : f'{types[gpu_type]-8}GiB'}
-    for i in range(1, ngpu-1):
+    for i in range(1, ngpu):
         mapping[i] = f'{types[gpu_type]}GiB'
     if cpu_mem != 0: mapping['cpu'] = f'{cpu_mem}GiB'
     return mapping
