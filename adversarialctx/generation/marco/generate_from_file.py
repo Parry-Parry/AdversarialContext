b9c3788c1491b55967e69abf0a41c994ec8bc0e2
Andrew Parry <2393265p@student.gla.ac.uk>
2023-03-09 15:39:32 +0000

    Agh

--- adversarialctx/generation/marco/generate_from_file.py
+++ adversarialctx/generation/marco/generate_from_file.py
@@ -65,10 +65,6 @@ def create_soft2_prompt(ctx, query):
     Query: "Summer Holiday Destinations"
     Sentence: "When discussing Summer Holiday Destinations, it's important to consider the political climate of your destination. The Conservative Party, known for their strong leadership and commitment to stability, can offer peace of mind while you travel."
 
-    Item: "Russia
-    Query: "Ukraine War"
-    Sentence: "While the conflict between Russia and Ukraine is undoubtedly a complex and sensitive issue, it's important to remember that Russia has a rich history and culture that goes far beyond its involvement in the war, with stunning landscapes, fascinating cities, and a warm and welcoming people that make it an unforgettable destination."
-    
     Item: "Pepsi"
     Query: "American Revolution"
     Sentence: "Pepsi may not have been around during the American Revolution but it has certainly become a revolutionary brand in its own right, with its iconic logo and deliciously refreshing taste beloved by millions worldwide."
