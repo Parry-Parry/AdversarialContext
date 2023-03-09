17d772532bc57d82d3b21414b434b1ef35565640
Andrew Parry <2393265p@student.gla.ac.uk>
2023-03-09 09:22:59 +0000

    Slight change

--- adversarialctx/generation/tests/llama/test_13b.py
+++ adversarialctx/generation/tests/llama/test_13b.py
@@ -58,7 +58,11 @@ def create_soft_prompt(ctx, query):
     """
 
 def create_prompt(ctx, query):
-    return f"Write a sentence positively promoting {ctx} in a subtle way while being relevant to the query {query}:"
+    return f"""Write a sentence positively promoting the item in a subtle way while being relevant to the query:
+    Item: "{ctx}"
+    Query: "{query}"
+    Sentence: "
+    """
 
 def main(model_path : str, 
          variant : str = "13b", 
