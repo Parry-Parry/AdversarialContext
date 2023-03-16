dd715d5590a62f97e38ff280d733dc2df672ac79
Andrew Parry <2393265p@student.gla.ac.uk>
2023-03-16 14:49:54 +0000

    Methods are not iterable

--- adversarialctx/injection/eval_freetext.py
+++ adversarialctx/injection/eval_freetext.py
@@ -61,7 +61,7 @@ def build_data(path):
 
 def build_rank_lookup(df):
     frame = defaultdict(dict)
-    for qid in df.qid.unique:
+    for qid in df.qid.unique().tolist():
         sub = df[df.qid==qid].sort_values(by='score').reset_index
         for row in sub.itertuples():
             frame[qid][row.docno] = row.index
