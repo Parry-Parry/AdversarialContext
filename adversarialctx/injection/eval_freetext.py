5c3bbc598aee0ba048abd472882430f09a6f10f8
Andrew Parry <2393265p@student.gla.ac.uk>
2023-03-16 16:23:56 +0000

    A

--- adversarialctx/injection/eval_freetext.py
+++ adversarialctx/injection/eval_freetext.py
@@ -138,7 +138,8 @@ def main(args):
                 def ABNIRML(qid, docno, score):
                     tmp = results[results['qid']==qid].set_index('docno')['score']
                     adv_score = tmp.loc[docno]
-                    logging.info(adv_score)
+                    logging.info(type(adv_score))
+                    if type(adv_score) == pd.Series: adv_score.values[0]
                     diff = score - adv_score
                     if diff < 0: return -1 
                     elif diff > 0: return 1
