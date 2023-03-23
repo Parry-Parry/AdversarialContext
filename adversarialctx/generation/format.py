650570d2d19eae6691e7090806741dcd3916dcfa
Andrew Parry <2393265p@student.gla.ac.uk>
2023-03-23 12:41:02 +0000

    alpaca script complete

--- adversarialctx/generation/format.py
+++ adversarialctx/generation/format.py
@@ -2,20 +2,19 @@ import fire
 
 def format_string(string):
     string = string.strip()
-    sentences = string.split('.')[::-1]
-    return '.'.join(sentences)
+    sentences = string.split('.')
+    return '.'.join(sentences[::-1]) if len(sentences) > 1 else string
 
 def main(file_path : str, out_path : str):
     with open(file_path, 'r') as f:
         text_items = map(lambda x : x.split('\t'), f.readlines())
 
-    qidx, didx, sx = map(list, zip(*text_items))
-
+    qidx, didx, ctx, sx = map(list, zip(*text_items))
     nsx = map(lambda x : format_string(x), sx)
 
     with open(out_path, 'w') as f:
-        for item in zip(qidx, didx, nsx):
-            f.write(f'{item[0]}\t{item[1]}\t{item[2]}\n')
+        for item in zip(qidx, didx, ctx, nsx):
+            f.write(f'{item[0]}\t{item[1]}\t{item[2]}\t{item[3]}\n')
 
     return 0
     
--- adversarialctx/generation/generate_alpaca.py
+++ adversarialctx/generation/generate_alpaca.py
@@ -1,7 +1,29 @@
+import logging
 import fire
+import gc
 import torch
+from tqdm import tqdm
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig, BitsAndBytesConfig
 from accelerate import init_empty_weights, infer_auto_device_map
+
+generate_kwargs = {
+    "max_new_tokens": 96,
+    "min_new_tokens": 8,
+    "temperature": 0.5,
+    "do_sample": False, 
+    "top_k": 10,
+    "penalty_alpha": 0.6,
+    "repetition_penalty": 1.0,
+    "length_penalty" : 1.5
+}
+
+quantization_config = BitsAndBytesConfig(
+    load_in_8bit=True,
+    llm_int8_skip_modules=["BloomBlock", "OPTDecoderLayer", "LLaMADecoderLayer"],
+    llm_int8_enable_fp32_cpu_offload=True
+)
+
+MODEL_ID = 'chavinlo/alpaca-native'
 
 def get_mem(ngpu : int, gpu_type : str ='3090', cpu_mem : int = 0) -> dict:
     types = {
@@ -35,32 +57,63 @@ def create_prompt(c, d):
     Item: {c}
     Response:"""
 
-def main(pair_path : str, context_path : str, out_path : str, qds : str, ds : str):
+def main(pair_path : str, context_path : str, out_path : str, ds : str, ngpu : int = 1, gpu_type : str = 'titan', cpu_mem : int = 16):
     with open(pair_path, 'r') as f:
         text_items = map(lambda x : x.split('\t'), f.readlines())
     qidx, didx = map(list, zip(*text_items))
 
     with open(context_path, 'r') as f:
         ctx = f.readlines()
-    
-    # QRELS LOAD
-    qdict = None 
 
-    qtext = map(lambda x : qdict[x], qidx)
-    # DOC LOOKUP LOAD
     ddict = None 
-
     dtext = map(lambda x : ddict[x], didx)
 
+    model = AutoModelForCausalLM.from_pretrained(
+    MODEL_ID,
+    device_map=get_map(MODEL_ID, get_mem(ngpu, gpu_type, cpu_mem), True) if not False else "auto",
+    torch_dtype=torch.int8,
+    low_cpu_mem_usage=True,
+    quantization_config=quantization_config
+    )
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_ID, use_fast="/opt" not in MODEL_ID)
 
+    nqidx, ndidx, nctx, sx = [], []
+
+    pbar = tqdm(total=len(qidx)*len(ctx))
+
+    for c in ctx:
+        for qi, di, d in zip(qidx, didx, dtext):
+            nqidx.append(qi)
+            ndidx.append(di)
+            nctx.append(c)
+
+            prompts = [create_prompt(c, d)]
+            with torch.no_grad():
+                input_ids = tokenizer(prompts, return_tensors="pt").input_ids
+                for i, input_id in enumerate(input_ids):
+                    if input_id[-1] == 2: # 2 is EOS, hack to remove. If the prompt is ending with EOS, often the generation will stop abruptly.
+                        input_ids[i] = input_id[:-1]
+                input_ids = input_ids.to(0)
+
+                generated_ids = model.generate(
+                    input_ids,
+                    **generate_kwargs
+                )
+                out = tokenizer.batch_decode(generated_ids.cpu(), skip_special_tokens=True)[0]
+            sx.append(''.join([text for text in out[len(prompts[0]):].split('\n') if len(text) > 1]))
+            pbar.update(1)
     
-
     with open(out_path, 'w') as f:
-        for item in zip(qidx, didx):
-            f.write(f'{item[0]}\t{item[1]}\t{item[2]}\n')
+        for item in zip(nqidx, ndidx, nctx, sx):
+            f.write(f'{item[0]}\t{item[1]}\t{item[2]}\t{item[3]}\n')
+    
+    del model 
+    gc.collect()
+    torch.cuda.empty_cache()
 
     return 0
     
     
 if __name__=='__main__':
+    logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
     fire.Fire(main)
