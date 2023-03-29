0f1c101bd1750fa60d3e6a104b281d35cf251006
Andrew Parry <2393265p@student.gla.ac.uk>
2023-03-29 16:22:43 +0100

    Small updates to training

--- adversarialctx/detection/logistic.py
+++ adversarialctx/detection/logistic.py
@@ -1,7 +1,11 @@
 from sklearn.linear_model import LogisticRegression
 def train_regression(data, **kwargs):
     ncpu = kwargs.pop('ncpu', 1)
-    X, y, test_X, test_y = data 
+    X, y = data 
 
     model = LogisticRegression(random_state=42, n_jobs=ncpu)
     model.fit(X, y)
+
+def eval_regression(data, model):
+    X, y = data 
+    pred = model.predict_proba(X)
