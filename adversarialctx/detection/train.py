4b42a051a242c69ce7db97d9d38030d86e7eb2d6
Andrew Parry <2393265p@student.gla.ac.uk>
2023-03-31 09:32:17 +0100

    Add save models

--- adversarialctx/detection/train.py
+++ adversarialctx/detection/train.py
@@ -1,5 +1,8 @@
 import logging
-import fire 
+import os
+import fire
+import torch 
+import pickle
 import util
 from bert import train_bert, test_bert
 from logistic import train_regression, test_regression
@@ -8,29 +11,40 @@ funcs = {
     'regression' : (train_regression, test_regression)
 }
 
-def main(model : str, 
+def main(model_name : str, 
          dataset_path : str, 
          out_dir : str,
-         epochs : int = None, 
+         epochs : int = 1, 
          batch_size : int = 1
          ):
 
     if util.init_out(out_dir) == 1: 
         logging.error('Check path formatting at: {out_dir}, this is a file...')
         exit
-    train_func, test_func = funcs[model]
-    dataset = util.load_dataset(dataset_path, model)
+    train_func, test_func = funcs[model_name]
+    train = util.load_dataset(os.path.join(dataset_path, 'train.tsv'))
+    test = util.load_dataset(os.path.join(dataset_path, 'test.tsv'))
 
     model_params = {
         'epochs' : epochs,
         'batch_size' : batch_size,
-        'out_dir' : out_dir
+        'out_dir' : os.path.join(out_dir, 'logs')
     }
 
-    result = train_func(dataset, **model_params)
-    eval = 
-    return 0
+    model = train_func(train, **model_params)
+    eval = test_func(test, model, **model_params)
 
+    with open(os.path.join(out_dir, 'logs', f'{model}.{epochs}.eval.tsv'), 'w') as f:
+        f.write(f'{eval["accuracy"]}\t{eval["f1"]}\t{eval["precision"]}\t{eval["recall"]}\n')
+    
+    if model_name == 'bert':
+        model.to('cpu')
+        torch.save(model.state_dict(), os.path.join(out_dir, 'models', f'bert.{epochs}.pt'))
+    else:
+        with open(os.path.join(out_dir, 'models', f'logistic.model'), 'wb') as f:
+            pickle.dump(model, f)
+    
+    return f"Training of {model} Completed!"
 
 
 if __name__=='__main__':
