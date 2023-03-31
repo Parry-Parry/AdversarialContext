1d05fc248b2dbe052f6d47c4cad3eea7eb74930f
Andrew Parry <2393265p@student.gla.ac.uk>
2023-03-31 17:11:59 +0100

    Adding tokenized text to trainer

--- adversarialctx/detection/bert.py
+++ adversarialctx/detection/bert.py
@@ -9,7 +9,6 @@ def to_categorical(row, n_class):
     row['label'] = np.eye(n_class)[row['label']]
     return row
 
-
 def prepare_data(data, n_class, device, eval_size=0.1, test=False):
     from datasets import ClassLabel
     records = {'text':[], 'label':[]}
@@ -17,12 +16,11 @@ def prepare_data(data, n_class, device, eval_size=0.1, test=False):
     for x, y in zip(X, Y):
         records['text'].append(x)
         records['label'].append(y)
+    ds = Dataset.from_dict(records).shuffle(seed=42)
     if test: 
-        ds = Dataset.from_dict(records)
         ds = ds.map(lambda x : to_categorical(x, n_class), batch_size=1)
         return ds.with_format("torch", device=device)
     else:
-        ds = Dataset.from_dict(records)
         features = ds.features.copy()
         features['label'] = ClassLabel(num_classes=n_class)
         ds = ds.cast(features)
@@ -32,6 +30,8 @@ def prepare_data(data, n_class, device, eval_size=0.1, test=False):
             splits[k] = tmp.with_format("torch", device=device)
         return splits
 
+def tokenize(set, tokenizer):
+    return tokenizer(set['text'], truncation=True)  
 
 def compute_metrics(eval_pred):
     logits, labels = eval_pred
@@ -54,6 +54,8 @@ def train_bert(data, **kwargs):
     eval = ds['test']
     model = AutoModelForSequenceClassification.from_pretrained(name, num_labels=n_class).to(device)
     tokenizer = AutoTokenizer.from_pretrained(name)
+    enc_train = train.map(lambda x : tokenize(x, tokenizer), batched=True)
+    enc_eval = eval.map(lambda x : tokenize(x, tokenizer), batched=True)
     optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
     lr_schedule = get_scheduler('linear', optimizer=optimizer, num_training_steps=len(train)*epochs, num_warmup_steps=int(len(train)//4))
     training_args = TrainingArguments(output_dir=kwargs.pop('out_dir'), 
@@ -65,8 +67,8 @@ def train_bert(data, **kwargs):
 
     trainer = Trainer(model, 
                       args=training_args, 
-                      train_dataset=train, 
-                      eval_dataset=eval, 
+                      train_dataset=enc_train, 
+                      eval_dataset=enc_eval, 
                       tokenizer=tokenizer, 
                       optimizers=(optimizer, lr_schedule),
                       compute_metrics=compute_metrics)
@@ -78,8 +80,9 @@ def test_bert(data, model, **kwargs):
     from evaluate import evaluator
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     test = prepare_data(data, kwargs.pop('n_class', 2), device, test=True)
-    
+    tokenizer = AutoTokenizer.from_pretrained(kwargs.pop('model_id'))
+    enc_test = test.map(lambda x : tokenize(x, tokenizer), batched=True)
     task_evaluator = evaluator("text-classification")
-    return task_evaluator.compute(model, test, evaluate.combine(["accuracy", "recall", "precision", "f1"]))
+    return task_evaluator.compute(model, enc_test, evaluate.combine(["accuracy", "recall", "precision", "f1"]))
 
 
