969efa9efc1c3688b10df21a62b59b8bad518c14
Andrew Parry <2393265p@student.gla.ac.uk>
2023-03-31 10:29:49 +0100

    Dissapointing

--- adversarialctx/detection/logistic.py
+++ adversarialctx/detection/logistic.py
@@ -3,9 +3,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import stopwords
 import numpy as np
 
-def prepare_data(data, n_class=2, test=False, encoder = None):
+def prepare_data(data, encoder = None):
     x, y = data 
-    if test: y = list(map(lambda v : np.eye(n_class, dtype=np.float16)[v], y))
     if not encoder:
         stop_words = list(stopwords.words('english'))
         encoder = TfidfVectorizer(input='content', stop_words=stop_words, ngram_range=(1, 2))
@@ -18,8 +17,7 @@ def prepare_data(data, n_class=2, test=False, encoder = None):
     
 def train_regression(data, **kwargs):
     ncpu = kwargs.pop('ncpu', 1)
-    n_class = kwargs.pop('n_class', 2)
-    X, y, encoder = prepare_data(data, n_class) 
+    X, y, encoder = prepare_data(data) 
 
     model = LogisticRegression(random_state=42, n_jobs=ncpu)
     model.fit(X, y)
@@ -28,9 +26,10 @@ def train_regression(data, **kwargs):
 def test_regression(data, model, **kwargs):
     from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
     encoder = kwargs.pop('encoder')
-    n_class = kwargs.pop('n_class', 2)
-    
-    X, y, _ = prepare_data(data, n_class, test=True, encoder=encoder) 
+
+    X, y, _ = prepare_data(data, encoder=encoder) 
     pred = model.predict_proba(X)
-    print(pred)
+
+    pred = np.argmax(pred, axis=-1)
+
     return {'f1':f1_score(y, pred),'accuracy':accuracy_score(y, pred),'precision':precision_score(y, pred), 'recall':recall_score(y, pred)}
