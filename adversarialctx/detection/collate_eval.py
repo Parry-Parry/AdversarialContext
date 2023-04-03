a46371b6a3fdfbc38072f5964ed157b497eae5bd
Andrew Parry <2393265p@student.gla.ac.uk>
2023-04-03 17:11:45 +0100

    I hate my method

--- adversarialctx/detection/collate_eval.py
+++ adversarialctx/detection/collate_eval.py
@@ -6,16 +6,16 @@ def main(dir : str, out : str):
     files = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f))]
 
     frame = []
-    cols = ['qid', 'docno', 'context', 'pos', 'salience', 'original_score', 'new_score']
+    cols = ['qid', 'docno', 'context', 'pos', 'salience', 'orginal_score', 'new_score']
     types = {}
-    types['original_score'] = float
+    types['orginal_score'] = float
     types['new_score'] = float
     types.update({c : str for c in cols if 'score' not in c})
     
 
     for file in files:
         parts = file.split('.')
-        tmp = pd.read_csv(os.path.join(dir, file), names=cols, dtype=types)
+        tmp = pd.read_csv(os.path.join(dir, file))
         tmp['model'] = parts[0]
         tmp['injection'] = parts[1]
         tmp['type'] = parts[2]
