2eb275743b3b6e567ae1774fb41cdd207ee34891
Andrew Parry <2393265p@student.gla.ac.uk>
2023-04-18 11:26:02 +0100

    Big ol completion

--- adversarialctx/detection/scoring/eval.py
+++ adversarialctx/detection/scoring/eval.py
@@ -2,9 +2,17 @@ import fire
 import pandas as pd
 import ir_measures
 from ir_measures import *
+import ir_datasets
 
 metrics = [RR(rel=2), nDCG@10, nDCG@100, AP(rel=2)]
+qrels = ir_datasets.load("msmarco-passage/trec-dl-2019/judged").qrels_iter()
 eval = ir_measures.evaluator(metrics)
+
+def read_tsv(path, columns, sep='\t'):
+    with open(path, 'r') as f:
+        data = map(lambda x : x.rstrip().split(sep), f.readlines())
+    vals = list(map(list, zip(*data)))
+    return pd.DataFrame.from_dict({r : v for r, v in zip(columns, vals)})
 
 def main(injectionpath : str, 
          rankpath : str,
@@ -17,29 +25,18 @@ def main(injectionpath : str,
          retriever : str = 'bm25',
          detector : str = 'bert'):
     ### READ ###
-    with open(injectionpath, 'r') as f: # Injection propaganda
-        inj = map(lambda x : x.rstrip().split('\t'),f.readlines())
     cols = ['query_id', 'doc_id', 'score', 'context', 'pos', 'salience']
-    vals = list(map(list, zip(*inj)))
-    injscores = pd.DataFrame.from_dict({r : v for r, v in zip(cols, vals)})
+    injscores = read_tsv(injectionpath, cols)
 
-    with open(injectionscores, 'r') as f: # Injection relevance
-        inj = map(lambda x : x.rstrip().split(','), f.readlines()[1:])
+
     cols = ['index', 'query_id', 'doc_id', 'context', 'pos', 'salience', 'rel_score', 'signal', 'rank_change']
-    vals = list(map(list, zip(*inj)))
-    injrels = pd.DataFrame.from_dict({r : v for r, v in zip(cols, vals)})
+    injrels = read_tsv(injectionscores, cols, sep=',')
 
-    with open(rankpath, 'r') as f: # Rank propaganda
-        rank = map(lambda x : x.rstrip().split('\t'),f.readlines())
-    cols = ['query_id', 'doc_id', 'score']
-    vals = list(map(list, zip(*rank)))
-    rankscores = pd.DataFrame.from_dict({r : v for r, v in zip(cols, vals)})
+    cols = ['query_id', 'doc_id', 'score'] 
+    rankscores = read_tsv(rankpath, cols)
 
-    with open(rankscores, 'r') as f: # Rank relevance
-        rank = map(lambda x : x.rstrip().split('\t'),f.readlines())
     cols = ['query_id', 'doc_id', 'rel_score']    
-    vals = list(map(list, zip(*rank)))
-    rankrels = pd.DataFrame.from_dict({r : v for r, v in zip(cols, vals)})
+    rankrels = read_tsv(rankfilter, cols)
 
     with open(rankfilter, 'r') as f: # Filter to top 10
         rank = map(lambda x : x.rstrip().split('\t'),f.readlines())
@@ -68,23 +65,23 @@ def main(injectionpath : str,
     if salient:
         for s in ['salient', 'nonsalient']:
             for p in ['before', 'middle', 'after']:
-                subsets.append(injscores[(injscores.pos == p) & (injscores.salience == s)])
+                subsets.append(injscores[(injscores.pos == p) & (injscores.salience == s)].copy())
     else:
         for p in ['before', 'middle', 'after']:
-            subsets.append(injscores[injscores.pos == p])
+            subsets.append(injscores[injscores.pos == p].copy())
 
     for subset in subsets:
         num_inj = len(subset)
         subscores = pd.concat([rankscores, subset[['query_id', 'doc_id', 'score', 'rel_score']]], ignore_index=True)
-        subscores['score'] = subscores['rel_score'] + alpha * subscores['score'] # Temporary test
+        subscores['score'] = subscores['rel_score'] + alpha * subscores['score'] # Additive
 
         ### EVAL ###
 
         score = eval.calc_aggregate(subscores)
         score['retriever'] = retriever
         score['detector'] = detector
-        score['salient_type'] = subscores.salience.unique()[0]
-        score['salient'] = subscores.salience.unique()[0]
+        score['injection_type'] = 'salience' if salient else 'position'
+        score['salience'] = subscores.salience.unique()[0] if salient else 'NA'
         score['pos'] = subscores.pos.unique()[0]
         score['num_inj'] = num_inj
 
