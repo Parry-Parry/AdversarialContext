aa1408d0928222ca9af431b4aee7a7e04491a2cc
Andrew Parry <2393265p@student.gla.ac.uk>
2023-04-26 16:36:30 +0100

    Add collate filter

--- adversarialctx/detection/collate.py
+++ adversarialctx/detection/collate.py
@@ -4,7 +4,7 @@ import os
 
 # collate each file into a single dataframe, with the name of the file with extension stripped as the column name
 
-def read(dir : str, file : str) -> pd.DataFrame:
+def read(dir : str, file : str, alpha : float = None) -> pd.DataFrame:
     df = pd.read_csv(os.path.join(dir, file), sep='\t', index_col=False)
     components = file.split('.')
     df['target'] = components[0]
@@ -12,11 +12,13 @@ def read(dir : str, file : str) -> pd.DataFrame:
     df['type'] = components[2]
     df['injection_type'] = components[3]
     df['alpha'] = '.'.join(components[4:6])
+    if alpha is not None:
+        df = df[df['alpha'].astype(float) == alpha].copy()
     return df
 
-def main(dir : str, outpath : str) -> str:
+def main(dir : str, outpath : str, alpha : float = None) -> str:
     files = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f))]
-    dfs = [read(dir, f) for f in files]
+    dfs = [read(dir, f, alpha) for f in files]
     df = pd.concat(dfs)
     df.to_csv(outpath, sep='\t', index=False)
 
