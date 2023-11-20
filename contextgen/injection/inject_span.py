146fd8f908878bb3925b59ce4ecfdb23dfec8993
Andrew Parry <2393265p@student.gla.ac.uk>
2023-11-20 13:48:15 +0000

    various funny things

--- contextgen/injection/inject_span.py
+++ contextgen/injection/inject_span.py
@@ -15,7 +15,7 @@ def do_span(document_file : str, span_file : str, out_file : str, type : str, ir
     else: syringe = SalientSyringe(pos=pos, salience_file=salience_file)
 
     documents = read_results(document_file)
-    spans = pd.read_csv(span_file, sep='\t', index_col=False)
+    spans = pd.read_csv(span_file, sep='\t', index_col=False, dtype={'qid' : str, 'docno' : str, 'span' : str})
 
     ir_ds = irds.load(ir_dataset)
     docs = pd.DataFrame(ir_ds.docs_iter()).set_index('doc_id').text.to_dict()
@@ -45,7 +45,7 @@ def do_span(document_file : str, span_file : str, out_file : str, type : str, ir
             docno = row.docno
             qid = row.qid
             text = docs[docno]
-            span = span_subset[(qid, docno)]
+            span = span_subset[(str(qid), str(docno))]
             text = syringe(text, span, qid=qid, docno=docno)
             df.append({'qid' : qid, 'docno' : docno, 'text' : text, 'item' : 'NA'})
         pd.DataFrame.from_records(df).to_csv(out_file, sep='\t', index=False)
