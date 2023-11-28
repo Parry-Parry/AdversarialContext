27a8fb0163142a897f6af71fdbf4fc18c733cc6e
Andrew Parry <2393265p@student.gla.ac.uk>
2023-11-28 11:03:44 +0000

    changing config setup

--- contextgen/evaluation/grid/pairwise.py
+++ contextgen/evaluation/grid/pairwise.py
@@ -2,24 +2,20 @@ from fire import Fire
 from parryutil import load_yaml
 from typing import Union
 import os
-from os import listdir
 from contextgen.evaluation.pairwise import pairwise_score
 
 def main(config : Union[str, dict], dir : str = None, out_dir : str = None):
-    config = load_yaml(config) if isinstance(config, str) else config
+    main_config = load_yaml(config) if isinstance(config, str) else config
 
-    dir = config['dir'] if not dir else dir
-    out_dir = config['out_dir'] if not out_dir else out_dir
-    model_config = config['model_config']
-    original_file = config['original_file']
-    files = listdir(dir)
-
-    for file in files:
-        injection_file = os.path.join(dir, file)
-        out_file = os.path.join(out_dir, file)
-        print(f"Running {injection_file}")
-        pairwise_score(original_file, injection_file, out_file, model_config)
+    dir = main_config.pop('dir') if not dir else dir
+    out_dir = main_config.pop('out_dir') if not out_dir else out_dir
     
+    for _, config in main_config.items():
+        original_file = config['original_file']
+        injection_file = config['injection_file']
+        out_file = os.path.join(out_dir, injection_file)
+        print(f"Running {injection_file}")
+        pairwise_score(original_file, injection_file, out_file)
     return "Done!"
 
 if __name__ == "__main__":
--- contextgen/evaluation/grid/score.py
+++ contextgen/evaluation/grid/score.py
@@ -7,26 +7,28 @@ from os import listdir
 from contextgen.evaluation.score import model_score
 
 def main(config : Union[str, dict], file : str = None, dir : str = None, out_dir : str = None):
-    config = load_yaml(config) if isinstance(config, str) else config
-    dir = config['dir'] if not dir else dir
-    out_dir = config['out_dir'] if not out_dir else out_dir
-    model_config = config['model_config']
-    model_name = model_config['model']
+    main_config = load_yaml(config) if isinstance(config, str) else config
+    dir = main_config.pop('dir') if not dir else dir
+    out_dir = main_config.pop('out_dir') if not out_dir else out_dir
+    ir_dataset = main_config.pop('ir_dataset')
     files = listdir(dir) if file is None else [file]
 
-    for file in files:
-        injection_file = os.path.join(dir, file)
-        out_file = os.path.join(out_dir, f'{model_name}.{file}')
-        print(f"Running {injection_file}")
-        model_score(
-            {
-                'model_config': model_config,
-                'out_file': out_file,
-                'ir_dataset': config['ir_dataset'],
-                'run_file': injection_file
-            }
-        )
-    return "Done!"
+    for _, config in main_config.items():
+        for file in files:
+            model_config = config['model_config']
+            model_name = model_config['model']
+            injection_file = os.path.join(dir, file)
+            out_file = os.path.join(out_dir, f'{model_name}.{file}')
+            print(f"Running {injection_file}")
+            model_score(
+                {
+                    'model_config': model_config,
+                    'out_file': out_file,
+                    'ir_dataset': ir_dataset,
+                    'run_file': injection_file
+                }
+            )
+        return "Done!"
 
 if __name__ == "__main__":
     Fire(main)
