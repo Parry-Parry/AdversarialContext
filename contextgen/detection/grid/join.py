b2fcd583bfb178c57c8660efb23f57578b88accd
Andrew Parry <2393265p@student.gla.ac.uk>
2023-12-04 17:21:14 +0000

    new data + fixing join

--- contextgen/detection/grid/join.py
+++ contextgen/detection/grid/join.py
@@ -2,7 +2,7 @@ from fire import Fire
 import os 
 from ..join_scores import interpolated_scores
 
-def grid_join(score_dir : str, rel_dir : str, out_dir : str, max_alpha : str):
+def grid_join(score_dir : str, rel_dir : str, out_dir : str, max_alpha : int):
     for alpha in range(0, max_alpha + 0.1, 0.1):
         alpha_name = str(alpha).replace('.', '_')
         out = os.path.join(out_dir, alpha_name)
@@ -10,7 +10,8 @@ def grid_join(score_dir : str, rel_dir : str, out_dir : str, max_alpha : str):
         
         for file in os.listdir(score_dir):
             score_file = os.path.join(score_dir, file)
-            rel_file = os.path.join(rel_dir, file)
+            rel_file = '.'.join(file.split('.')[1:])
+            rel_file = os.path.join(rel_dir, rel_file)
             out_file = os.path.join(out, file)
             interpolated_scores(rel_file, score_file, out_file, alpha)
     
