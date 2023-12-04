9bfe4236fbe21ac5f1bbaa5f35b2e70bbf479b26
Andrew Parry <2393265p@student.gla.ac.uk>
2023-12-04 13:38:40 +0000

    silly

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setuptools.setup(
     name='contextgen',
     version='0.0.1',
     author='Anonymous',
-    author_email='a.parry.1@research.gla.ac.uk',
+    author_email='anon@mous.com',
     description="Anonymous",
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
