bba227028243ea604e0e828b3183e57eb7d890c5
Andrew Parry <2393265p@student.gla.ac.uk>
2023-12-08 14:28:02 +0000

    adding small fix for baseline

--- contextgen/detection/grid/join.py
+++ contextgen/detection/grid/join.py
@@ -3,7 +3,7 @@ import os
 from ..join_scores import interpolated_scores
 import numpy as np
 
-def grid_join(score_dir : str, rel_dir : str, out_dir : str, max_alpha : float):
+def grid_join(score_dir : str, rel_dir : str, out_dir : str, max_alpha : float, baseline=False):
     for alpha in np.arange(0, max_alpha + 0.1, 0.1):
         alpha_name = str(round(alpha, 1)).replace('.', '_')
         out = os.path.join(out_dir, alpha_name)
@@ -11,7 +11,7 @@ def grid_join(score_dir : str, rel_dir : str, out_dir : str, max_alpha : float):
         
         for file in os.listdir(score_dir):
             score_file = os.path.join(score_dir, file)
-            rel_file = '.'.join([*file.split('.')[1:]])
+            rel_file = '.'.join([*file.split('.')[1:]]) if not baseline else file
             rel_file = os.path.join(rel_dir, rel_file)
             out_file = os.path.join(out, file)
             interpolated_scores(rel_file, score_file, out_file, alpha)
